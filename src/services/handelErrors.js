92468b7dfa8adc9ead5f5f8821691bb403f38916
Mones Hamd <mones.m.azzam91@gmail.com>
2022-07-18 03:22:07 +0200

    make SIRRA responsive and style error page

--- src/services/handelErrors.js
+++ src/services/handelErrors.js
@@ -2,9 +2,15 @@ import { initButtonElement } from '../pages/backButtonPage.js';
 import { initNewPage } from './initNewPage.js';
 
 export const renderError = (err) => {
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'error-container';
+  const errorImage = document.createElement('img');
+  errorImage.src = 'public/img/error.jpeg';
   const errorEl = document.createElement('h1');
   errorEl.className = 'error';
   errorEl.innerHTML = `Something went wrong ${err} try again later`;
-  initNewPage(errorEl);
+  errorDiv.appendChild(errorEl);
+  errorDiv.appendChild(errorImage);
+  initNewPage(errorDiv);
   initButtonElement();
 };
